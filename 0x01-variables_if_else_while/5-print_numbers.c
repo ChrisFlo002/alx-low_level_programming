9c28b12554c5fd3e490605ba19fcd382432d8b43
ChrisFlo002 <111631471+ChrisFlo002@users.noreply.github.com>
2022-12-11 08:19:20 -0800

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - Prints all numbers of base 10
+* Return: 0
+**/
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		printf("%d\n", num);
+	}
+	return (0);
+}
