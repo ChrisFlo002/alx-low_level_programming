0332889fad3b3816a98c93dce5a0df411593ef88
ChrisFlo002 <111631471+ChrisFlo002@users.noreply.github.com>
2022-12-11 09:31:45 -0800

    bug fix

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,11 +14,11 @@ for (b = 0; b < 100; b++)
 {
 if (a < b && a != b)
 {
-putchar((a / 10));
-putchar((a % 10));
+putchar((a / 10) + '0');
+putchar((a % 10) + '0');
 putchar(' ');
-putchar ((b / 10));
-putchar ((b % 10));
+putchar ((b / 10) + '0');
+putchar ((b % 10) + '0');
 if (!(a == 98 && b == 99))
 {
 putchar(',');
