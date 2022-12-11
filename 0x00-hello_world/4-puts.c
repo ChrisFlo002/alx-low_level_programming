45d7f3adc2588650ddf17b4aaebe50e13794d458
ChrisFlo002 <floriachristie2019@gmail.com>
2022-12-11 03:57:35 -0800

    bug fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-gcc -c $CFILE1 -o puts
+#include<stdio.h>
+/*
+ * use of puts()
+ */
+int main(void)
+{
+	char str[50] = 'Programming is like building a multilingual puzzle';
+	puts(str);
+	return(0);
+}	
