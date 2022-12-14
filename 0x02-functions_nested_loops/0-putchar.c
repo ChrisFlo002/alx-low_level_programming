74db54d2707c333f82238155072b12a5fda202a0
ChrisFlo002 <111631471+ChrisFlo002@users.noreply.github.com>
2022-12-14 10:38:40 -0800

    update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,20 @@
-#include "main.h"
-/*
- * main - print "_putchar" followed by a new line
- * Description: program should return 0
- * Return: Always 0 (success)
+#include "holberton.h"
+/**
+ * main - Print "Holberton" followed by a new line.
+ * Description: You are not allowed to include standard libraries.
+ * Return: 0
  */
 int main(void)
 {
-	printf("_putchar\n");
+	char holberton[] = "_putchar";
+	int i = 0;
+
+	while (holberton[i] != '\0')
+	{
+		_putchar(holberton[i]);
+		i++;
+	}
+	_putchar('\n');
+
 	return (0);
 }
