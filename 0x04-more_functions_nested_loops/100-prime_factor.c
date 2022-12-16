29470b4085c8a8ece5ab5a53ea96b157cd5584db
ChrisFlo002 <111631471+ChrisFlo002@users.noreply.github.com>
2022-12-16 09:38:42 -0800

    update

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -7,14 +7,6 @@
 * Return: 0 if no errors
 * A: Sqrt is a double by default so I did this to get rid of decimals.
 * B: While the number is bigger than the sqrt of the original number.
-* Why? Because if the number is prime my loop will go until the number.
-* If it has a factor then we know the largest possible prime factor
-* it can have, is less than the sqrt of the original number.
-* Math concept here, don’t need to check full length
-* saves lots of time to log n I think. This is a hard concept
-* UPDATE: After test cases, i changed it from number > sqrt to
-* number > i because some small test cases did not pass 
-* But with the change it should work
 * C: If the current number can be modulo’d by i, check if i is bigger
 * than biggest. We then chunk the number by the i, reducing it and
 * running the loop with the same i to further chuck it down, if possible.
