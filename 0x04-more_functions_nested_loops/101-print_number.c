4addfe80d3654fcfe36413bbffae859db8bba884
ChrisFlo002 <111631471+ChrisFlo002@users.noreply.github.com>
2022-12-16 10:06:05 -0800

    update

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -41,40 +41,6 @@ void print_number(int n)
 		x = -n;
 		_putchar('-');
 	}
-	if (n == INT_MAX || n == INT_MIN) /* D */
-	{
-		while (1)
-		{
-			if (n == INT_MAX)
-			{
-				_putchar('2');
-				_putchar('1');
-				_putchar('4');
-				_putchar('7');
-				_putchar('4');
-				_putchar('8');
-				_putchar('3');
-				_putchar('6');
-				_putchar('4');
-				_putchar('7');
-				break;
-			}
-			else if (n == INT_MIN)
-			{
-				_putchar('2');
-				_putchar('1');
-				_putchar('4');
-				_putchar('7');
-				_putchar('4');
-				_putchar('8');
-				_putchar('3');
-				_putchar('6');
-				_putchar('4');
-				_putchar('8');
-				break;
-			}
-		}
-	}
 	else if (n == 0)/* A */
 		_putchar('0');
 	else
@@ -88,4 +54,3 @@ void print_number(int n)
 			places /= 10;
 		}
 	}
-}
